a67ec03fb92c0e7722e3e9671a6f979a6c0c8022
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-07-10 00:53:35 -0300

    feat: add subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import SubscribeButton from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -13,6 +14,8 @@ export default function Home() {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get acess to all the publications <br /><span>for $9.90 month</span></p>
+
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="avatar" />
