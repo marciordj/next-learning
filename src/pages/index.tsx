fdc9814edd89a101d95e0c2a0169a3bca8bfee34
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-07-16 10:10:13 -0300

    feat: add stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import SubscribeButton from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface IHomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: IHomeProps) {
+
   return (
     <>
     <Head>
@@ -13,7 +23,7 @@ export default function Home() {
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
-          <p>Get acess to all the publications <br /><span>for $9.90 month</span></p>
+          <p>Get acess to all the publications <br /><span>for {product.amount} month</span></p>
 
           <SubscribeButton />
         </section>
@@ -22,4 +32,21 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JDpVnCfrJLjmKpNRDIYAnoC', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount / 100),
+  } 
+
+  return {
+    props: {
+      product
+    }
+  }
 }
