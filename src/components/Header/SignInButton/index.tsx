6d0c6903c3c73171e7f5f3541bb293126f77cac2
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-07-16 23:31:52 -0300

    feat: finish auth

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,19 +1,23 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 const SignInButton = () => {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+
+  console.log(session);
 
 
-  return isUserLoggedIn ? (
-    <button type='button' className={styles.signInButto}>
+  return session ? (
+    <button type='button' className={styles.signInButto} onClick={() => signOut()}>
       <FaGithub color="#04D361" />
-      Marcio Rodrigues
+      {session.user.name}
       <FiX color="#737380" className={styles.logOutIcon}/>
     </button>
   ) : (
-    <button type='button' className={styles.signInButto}>
+    <button type='button' className={styles.signInButto} onClick={() => signIn('github')}>
       <FaGithub color="#EBA417" />
       Sing in with GitHub
     </button>
